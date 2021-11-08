c37efff05d6cd67d9e531dae010b53241a81a40f
jesse <jesse152@163.com>
2021-11-09 07:59:45 +0800

    add button

--- vue3-demo/src/views/About.vue
+++ vue3-demo/src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+	<div class="about">
+		<h1>This is an about page</h1>
+		<button @click="goSubApp">go react</button>
+		<button @click="goMain">go main</button>
+	</div>
 </template>
+
+<script setup lang="ts">
+const goSubApp = () => {
+	history.pushState(null, "react-demo", "/react-demo");
+};
+
+const goMain = () => {
+	history.pushState(null, "home", "/home");
+};
+</script>
